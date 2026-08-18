999f90f8e7af01e20ac66adf6b9a409dc8ede46b
David <macistone71@gmail.com>
2026-08-18 23:26:07 +0800

    feat: 新增小红书概念卡片项目

---
+++ projects4.js
@@ -0,0 +1,9 @@
+// 项目 4：小红书概念卡片
+window.PROJECTS = window.PROJECTS || [];
+window.PROJECTS.push({
+  icon: "📕",
+  title: "小红书概念卡片",
+  desc: "一套可复用的小红书内容概念卡片模板，帮助快速产出结构化的内容创意。",
+  tags: ["内容创作", "小红书", "模板"],
+  link: "https://github.com/macistone71-jpg/xiaohongshu-concept-cards",
+});
